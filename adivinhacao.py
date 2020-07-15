449d38cd0092f05ee0d904019f2320d01aee054d
aline <ali.rodrigues>
2020-07-15 10:12:31 -0300

    melhorando o código com loop e aumentando as tentativas de chute

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,18 +2,29 @@ print("*********************************")
 print("Bem-Vindo ao jogo de adivinhação!")
 print("*********************************")
 
-numerosecreto = 42
+numero_secreto = 42
+total_tentivas = 3
+tentativa = 1
 
-chutestr = input("Digite um número: ")
+while(tentativa <= total_tentivas):
+    print("Tentativa", tentativa,  "de", total_tentivas)
+    chutestr = input("Digite um número: ")
+    chute = int(chutestr)
 
-chute = int(chutestr)
+    print("Você digitou o número:", chute)
 
-print("Você digitou o número: ", chute)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(chute == numerosecreto):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto")
+    tentativa = tentativa + 1
 
 print("***************")
 print("O jogo terminou")
