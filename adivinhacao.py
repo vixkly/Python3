a1a3fc1c643d9f856a8235f04817570173385a2c
aline <ali.rodrigues>
2020-07-15 10:49:59 -0300

    Alterando o laço While para for, arrumando erro de tentativa. Agora se a pessoa acerta de primeira o programa para

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,14 +4,16 @@ print("*********************************")
 
 numero_secreto = 42
 total_tentivas = 3
-tentativa = 1
 
-while(tentativa <= total_tentivas):
-    print("Tentativa", tentativa,  "de", total_tentivas)
-    chutestr = input("Digite um número: ")
+for tentativa in range(1, total_tentivas + 1):
+    print("Tentativa {} de {}".format(tentativa, total_tentivas))
+    chutestr = input("Digite um número entre 0 e 100:")
+    print("Você digitou o número:", chutestr)
     chute = int(chutestr)
 
-    print("Você digitou o número:", chute)
+    if(chute < 1 or chute > 100):
+        print("O número precisa ser maior que ou igual a 1 e menor que 100")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -19,12 +21,12 @@ while(tentativa <= total_tentivas):
 
     if(acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior do que o número secreto!")
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto")
-    tentativa = tentativa + 1
 
 print("***************")
 print("O jogo terminou")
